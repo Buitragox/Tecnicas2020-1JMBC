c8f980fc6440c559a9326b6f1974a7459ab27e9f
Buitragox <jhoanuitrago@gmail.com>
2020-04-12 14:55:28 -0500

    Añadir FuncionesCC.c

---
+++ Parcial2/FuncionesCC.c
@@ -0,0 +1,23 @@
+#include "FuncionesCC.h"
+
+local_t ** crearCentroComercial(int numPisos, int numLocalesxPiso){
+    local_t ** centroComercial = malloc(sizeof(local_t *) * numPisos);
+    int i;
+    if(centroComercial != NULL){
+        for(i = 0; i < numPisos; i++){
+            centroComercial[i] = malloc(sizeof(local_t) * numLocalesxPiso);
+            if(centroComercial[i] == NULL){
+                printf("Error reservando memoria");
+                break;
+            }
+        }
+    }
+    else{
+        printf("Error reservando memoria");
+    }
+    return centroComercial;
+}
+
+void mostrarLocal(local_t ** centroComercial, int numPisos, int numLocalesxPiso){
+	
+}
